57b92c02bd166c2621fe9dd5705d341ba9e5549b
Sur619 <whatyoulookingfor619@gmail.com>
2025-04-06 11:41:20 +0300

    upgrade settings, add nested settings

--- core/config.py
+++ core/config.py
@@ -1,10 +1,15 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
+
+
+class DBSettings(BaseModel):
+    url: str = "sqlite+aiosqlite:///./db.sqlite"
+    echo: bool = False  # print SQL queries to the console
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = "sqlite+aiosqlite:///./db.sqlite"
-    db_echo: bool = True  # print SQL queries to the console
+    db: DBSettings = DBSettings()
 
 
 settings = Settings()
